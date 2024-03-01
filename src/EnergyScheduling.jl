5eb3dd8a2c7179f15160ad700dcab920fb110f67
jorgelfer <jorgeferram17@gmail.com>
2024-03-01 12:23:22 -0500

    ignoring Manifest

--- src/EnergyScheduling.jl
+++ src/EnergyScheduling.jl
@@ -3,7 +3,6 @@ module EnergyScheduling
 # Write your package code here.
 using Expectations, Distributions
 import Pkg
-Pkg.add("LibGit2_jll")
 
 function foo(mu = 1., sigma = 2.)
     d = Normal(mu, sigma)
