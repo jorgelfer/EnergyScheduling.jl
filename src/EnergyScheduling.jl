39dbdc320db412ee34c0e7416c1074b4b12571e0
jorgelfer <jorgeferram17@gmail.com>
2024-03-01 12:20:44 -0500

    adding LibGit2_jll

--- src/EnergyScheduling.jl
+++ src/EnergyScheduling.jl
@@ -2,6 +2,8 @@ module EnergyScheduling
 
 # Write your package code here.
 using Expectations, Distributions
+import Pkg
+Pkg.add("LibGit2_jll")
 
 function foo(mu = 1., sigma = 2.)
     d = Normal(mu, sigma)
